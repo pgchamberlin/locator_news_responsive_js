8f2fcf1df19ae4f0eb0101e0a388f08d30af95a0
Darren Lucas <darren.lucas@bbc.co.uk>
2014-01-02 18:11:33 +0000

    LOCSERVICES-4109 - Making build simpler

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,52 +15,7 @@ module.exports = function( grunt ) {
           },
           optimize: "none" 
         }
-      },
-      vendorless: {
-        options: {
-          baseUrl: "src",
-          name: "locator/main",
-          out: "dist/locator.vendorless.js",
-          paths: {
-            "jquery": "empty:",
-          },
-          exclude: [
-            "vendor/istats/istats",
-            "vendor/events/pubsub.js"
-          ],
-          optimize: "none" 
-        }
-      },
-      desktop: {
-        options: {
-          baseUrl: "src",
-          name: "build.desktop",
-          out: "dist/locator.desktop.js",
-          paths: {
-            "jquery": "empty:",
-            "jquery-1": "empty:",
-            "locator/bootstrap": "locator/bootstrap_desktop"
-          },
-          optimize: "none"
-        }
-      },
-      desktop_vendorless: {
-        options: {
-          baseUrl: "src",
-          name: "build.desktop",
-          out: "dist/locator.desktop_vendorless.js",
-          paths: {
-            "jquery": "empty:",
-            "jquery-1": "empty:",
-            "locator/bootstrap": "locator/bootstrap_desktop"
-          },
-          exclude: [
-            "vendor/istats/istats",
-            "vendor/events/pubsub.js"
-          ],
-          optimize: "none"
-        }
-      },
+      }
     },
     jshint: {
       all: {
@@ -88,69 +43,6 @@ module.exports = function( grunt ) {
           preserveComments: false,
           sourceMap: "dist/locator.min.map",
           sourceMappingURL: "locator.min.map",
-          report: "min",
-          beautify: {
-            ascii_only: true
-          },
-          banner: "/*! Locator-js v<%= pkg.version %> | " +
-                  "(c) 2014 British Broadcasting Corporation. | ",
-          compress: {
-            hoist_funs: false,
-            loops: false,
-            unused: false
-          }
-        }
-      },
-      vendorless: {
-        files: {
-          "dist/locator.vendorless.min.js": [ "dist/locator.vendorless.js" ],
-        },
-        options: {
-          preserveComments: false,
-          sourceMap: "dist/locator.vendorless.min.map",
-          sourceMappingURL: "locator.vendorless.min.map",
-          report: "min",
-          beautify: {
-            ascii_only: true
-          },
-          banner: "/*! Locator-js v<%= pkg.version %> | " +
-                  "(c) 2014 British Broadcasting Corporation. | ",
-          compress: {
-            hoist_funs: false,
-            loops: false,
-            unused: false
-          }
-        }
-      },
-      desktop: {
-        files: {
-          "dist/locator.desktop.min.js": [ "dist/locator.desktop.js" ],
-        },
-        options: {
-          preserveComments: false,
-          sourceMap: "dist/locator.desktop.min.map",
-          sourceMappingURL: "locator.desktop.min.map",
-          report: "min",
-          beautify: {
-            ascii_only: true
-          },
-          banner: "/*! Locator-js v<%= pkg.version %> | " +
-                  "(c) 2014 British Broadcasting Corporation. | ",
-          compress: {
-            hoist_funs: false,
-            loops: false,
-            unused: false
-          }
-        }
-      },
-      desktop_vendorless: {
-        files: {
-          "dist/locator.desktop_vendorless.min.js": [ "dist/locator.desktop_vendorless.js" ],
-        },
-        options: {
-          preserveComments: false,
-          sourceMap: "dist/locator.desktop_vendorless.min.map",
-          sourceMappingURL: "locator.desktop_vendorless.min.map",
           report: "min",
           beautify: {
             ascii_only: true
